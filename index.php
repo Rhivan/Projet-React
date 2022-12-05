00a31f316538b6898d6696b41d23a937d19fc0af
Rhivan <apreynat@gaming.tech>
2022-12-05 18:02:07 +0100

    ,nbxklbw

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bébou</title>
+  </head>
+  <body>
+    <h1>Bébou</h1>
+    coucou toi !
+
+  </body>
+</html>
